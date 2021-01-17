9fbd96b24747e87f20b635cbf8abe55f3d016d3d
Rita Silva <rita.cfs28@gmail.com>
2021-01-17 22:58:18 +0000

    commit update

---
+++ src/intro/intro.js
@@ -0,0 +1,10 @@
+const casadoBtn = document.getElementById('CasadoBtn');
+const solteiroBtn = document.getElementById('SolteiroBtn');
+
+casadoBtn.addEventListener('click', function () {
+    location.href = './src/Casado/casado.html';
+});
+
+solteiroBtn.addEventListener('click', function () {
+    location.href = './src/Solteiro/solteiro.html'
+})
