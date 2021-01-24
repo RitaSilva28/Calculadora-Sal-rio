0c5c8cb464a45b7f6060d6e807971d71a2f9fa2d
Rita Silva <rita.cfs28@gmail.com>
2021-01-24 21:22:51 +0000

    paths

--- src/NaoCasado/naocasado.js
+++ src/NaoCasado/naocasado.js
@@ -1,6 +1,8 @@
 const radio1 = document.getElementById("inlineRadio1");
 const radio2 = document.getElementById("inlineRadio2");
 
+
+
 function checked() {
   console.log(radio1.checked);
   console.log(radio2.checked);
