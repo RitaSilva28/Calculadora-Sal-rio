61e62a9cbbb33752d1470b9a4f11ef4c43ab44d4
Rita Silva <rita.cfs28@gmail.com>
2021-01-26 22:00:54 +0000

    changes on paths and organization

---
+++ Casado/casado.js
@@ -0,0 +1,11 @@
+const titularBtn = document.getElementById("1TitularBtn");
+const titularesBtn = document.getElementById("2TitularesBtn");
+
+titularBtn.addEventListener("click", function () {
+    location.href = "/Casado/Titular/titular.html";
+
+});
+
+titularesBtn.addEventListener("click", function () {
+    location.href = "/Casado/Titulares/2titulares.html";
+});
